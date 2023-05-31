b63547af1c8c8bd4b8b275c980fb49c7de1b9245
golona73 <127319193+golona73@users.noreply.github.com>
2023-05-31 13:28:44 +0000

    6_1

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 class WrongStudentName extends Exception { }
+class WrongStudentAge extends Exception { }
 
 class Main {
     public static Scanner scan = new Scanner(System.in);
@@ -21,6 +22,11 @@ class Main {
             } catch(WrongStudentName e) {
                 System.out.println("Błędne imie studenta!");
             }
+          catch(WrongStudentAge e) {
+                System.out.println("Błędny wiek studenta!");
+          }
+        
+    
         }
     }
 
@@ -42,11 +48,20 @@ class Main {
 
         return name;
     }
-
-    public static void exercise1() throws IOException, WrongStudentName {
-        var name = ReadName();
+   public static int ReadAge() throws WrongStudentAge {
+        
         System.out.println("Podaj wiek: ");
-        var age = scan.nextInt();
+        int age = scan.nextInt();
+   
+        if( age>1 && age>100)
+            throw new WrongStudentAge();
+
+        return age;
+   }
+
+    public static void exercise1() throws IOException, WrongStudentName, WrongStudentAge{
+        var name = ReadName();
+        var age= ReadAge();
         scan.nextLine();
         System.out.println("Podaj datę urodzenia DD-MM-YYY");
         var date = scan.nextLine();
